90e25de97a69ade3c5fac63375c99bdfb03b4bad
Pouya Eghbali <persian.writer@gmail.com>
2017-09-28 23:36:48 +0330

    menu added

--- js/main.js
+++ js/main.js
@@ -36,6 +36,15 @@ $(document).ready(function () {
       dots: false,
       autoWidth: true
   })
+  $('#similar-products-slides').owlCarousel({
+      loop:true,
+      margin:10,
+      responsiveClass:true,
+      rtl: true,
+      nav:false,
+      dots: false,
+      autoWidth: true
+  })
   $('#brand-carousel').owlCarousel({
       loop:true,
       margin:10,
@@ -46,6 +55,7 @@ $(document).ready(function () {
       autoWidth: true,
   })
   $('#featured-products-slides .item .description').dotdotdot();
+  $('#similar-products-slides .item .description').dotdotdot();
   $('.search-results .item .description').dotdotdot();
   $('.filter-expander').each(function () {
     $(this).data('isOpen', false);
@@ -62,5 +72,49 @@ $(document).ready(function () {
       }
       $(this).data('isOpen', !isOpen);
     })
+  });
+  $('.menu-expander').each(function () {
+    $(this).data('isOpen', false);
+    $(this).click(function (e) {
+      e.preventDefault();
+      var $target = $(this).parent().find('.menu-link-collection');
+      var isOpen = $(this).data('isOpen');
+      if (isOpen) {
+        $(this).find('.chevron').html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+        $target.hide();
+      } else {
+        $(this).find('.chevron').html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+        $target.show();
+      }
+      $(this).data('isOpen', !isOpen);
+    })
+  });
+  $('.product-images-gallery img').each(function () {
+    $(this).mouseenter(function () {
+      var img = $(this).closest('.product-images').find('.product-image-display');
+      var src = $(this).attr('src');
+      img.css('opacity', 0);
+      setTimeout(function () {
+        img.attr('src', src).css('opacity', 1);
+      }, 200)
+    })
   })
+  $('.page').click(function () {
+    $('body').removeClass('menu-open');
+  })
+  $('.fixed-menu a').each(function () {
+    $(this).click(function (e) {
+      if ($(this).attr('href') != '#') {
+        return
+      }
+      $('.fixed-menu-menu-container').css('display', 'none');
+      $($(this).data('target')).css('display', 'block');
+      e.preventDefault();
+      $('body').addClass('menu-open');
+      e.stopPropagation();
+    });
+  })
+  $('.fixed-menu-contents').click(function (e) {
+    e.stopPropagation();
+  });
 })
